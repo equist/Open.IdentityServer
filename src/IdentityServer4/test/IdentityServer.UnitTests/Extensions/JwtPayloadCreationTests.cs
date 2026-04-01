11dc55d21b06d9ae301d2045e5f8f50fa3f5c0a7
James Britton <james.britton@rocksolidknowledge.com>
2026-04-01 18:14:51 +0100

    tests: for ignoring standar claim in payload creation

--- src/IdentityServer4/test/IdentityServer.UnitTests/Extensions/JwtPayloadCreationTests.cs
+++ src/IdentityServer4/test/IdentityServer.UnitTests/Extensions/JwtPayloadCreationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using AwesomeAssertions;
@@ -9,6 +10,8 @@ using IdentityServer4.Configuration;
 using IdentityServer4.Extensions;
 using IdentityServer4.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Time.Testing;
+using Microsoft.IdentityModel.Tokens;
 using Xunit;
 
 namespace IdentityServer.UnitTests.Extensions
@@ -19,7 +22,7 @@ namespace IdentityServer.UnitTests.Extensions
         
         public JwtPayloadCreationTests()
         {
-            var claims = new List<Claim>
+            List<Claim> claims = new List<Claim>
             {
                 new Claim(JwtClaimTypes.Scope, "scope1"),
                 new Claim(JwtClaimTypes.Scope, "scope2"),
@@ -39,11 +42,11 @@ namespace IdentityServer.UnitTests.Extensions
         [Fact]
         public void Should_create_scopes_as_array_by_default()
         {
-            var options = new IdentityServerOptions();
-            var payload = _token.CreateJwtPayload(TimeProvider.System, options, TestLogger.Create<JwtPayloadCreationTests>());
+            IdentityServerOptions options = new IdentityServerOptions();
+            JwtPayload payload = _token.CreateJwtPayload(TimeProvider.System, options, TestLogger.Create<JwtPayloadCreationTests>());
 
             payload.Should().NotBeNull();
-            var scopes = payload.Claims.Where(c => c.Type == JwtClaimTypes.Scope).ToArray();
+            Claim[] scopes = payload.Claims.Where(c => c.Type == JwtClaimTypes.Scope).ToArray();
             scopes.Count().Should().Be(3);
             scopes[0].Value.Should().Be("scope1");
             scopes[1].Value.Should().Be("scope2");
@@ -53,17 +56,46 @@ namespace IdentityServer.UnitTests.Extensions
         [Fact]
         public void Should_create_scopes_as_string()
         {
-            var options = new IdentityServerOptions
+            IdentityServerOptions options = new IdentityServerOptions
             {
                 EmitScopesAsSpaceDelimitedStringInJwt = true
             };
             
-            var payload = _token.CreateJwtPayload(TimeProvider.System, options, TestLogger.Create<JwtPayloadCreationTests>());
+            JwtPayload payload = _token.CreateJwtPayload(TimeProvider.System, options, TestLogger.Create<JwtPayloadCreationTests>());
 
             payload.Should().NotBeNull();
-            var scopes = payload.Claims.Where(c => c.Type == JwtClaimTypes.Scope).ToList();
+            List<Claim> scopes = payload.Claims.Where(c => c.Type == JwtClaimTypes.Scope).ToList();
             scopes.Count().Should().Be(1);
             scopes.First().Value.Should().Be("scope1 scope2 scope3");
+        }
+
+        [Fact]
+        public void Should_ignore_standard_validation_claims_in_token_claims()
+        {
+            IdentityServerOptions options = new IdentityServerOptions();
+            DateTime fakeNow = new DateTime(2026, 02, 19, 17, 00, 00, DateTimeKind.Utc);
+            FakeTimeProvider fakeTimeProvider = new();
+            fakeTimeProvider.SetUtcNow(fakeNow);
+
+            _token.Issuer = "https://fake.issuer.com";
+            _token.Audiences = ["api1", "apiC"];
+            _token.Lifetime = 20_000;
+
+            DateTime ignoredTime = new DateTime(2026, 04, 01, 01, 01, 01, DateTimeKind.Utc);
+            _token.Claims.Add(new Claim(JwtClaimTypes.Issuer, "https://updated.issuer.com"));
+            _token.Claims.Add(new Claim(JwtClaimTypes.Audience, ""));
+            _token.Claims.Add(new Claim(JwtClaimTypes.NotBefore, $"{EpochTime.GetIntDate(ignoredTime.ToUniversalTime())}", ClaimValueTypes.Integer64));
+            _token.Claims.Add(new Claim(JwtClaimTypes.Expiration, $"{EpochTime.GetIntDate(ignoredTime.AddDays(1).ToUniversalTime())}", ClaimValueTypes.Integer64));
+            
+            JwtPayload payload = _token.CreateJwtPayload(fakeTimeProvider, options, TestLogger.Create<JwtPayloadCreationTests>());
+
+            long expectedNotBefore = EpochTime.GetIntDate(fakeNow.ToUniversalTime());
+            long expectedExpiry = EpochTime.GetIntDate(fakeNow.AddSeconds(_token.Lifetime).ToUniversalTime());
+            
+            payload.Iss.Should().Be(_token.Issuer);
+            payload.Aud.Should().Contain(_token.Audiences);
+            payload.NotBefore.Should().Be(expectedNotBefore);
+            payload.Expiration.Should().Be(expectedExpiry);
         }
     }
 }
